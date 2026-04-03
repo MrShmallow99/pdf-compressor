d29e1b5e436c5cd7ffa345872f5b8330e292c6b6
Itamar <itamar@example.com>
2026-04-03 23:26:02 +0300

    fix: enforce local WASM loading and remove external CDN dependency

--- gs-worker.js
+++ gs-worker.js
@@ -701,11 +701,7 @@ function createExportWrapper(name) {
 
 // include: runtime_exceptions.js
 // end include: runtime_exceptions.js
-var wasmBinaryFile;
-  wasmBinaryFile = "https://cdn-wasm.b-cdn.net/gs-worker.wasm";
-  // if (!isDataURI(wasmBinaryFile)) {
-  //   wasmBinaryFile = locateFile(wasmBinaryFile);
-  // }
+var wasmBinaryFile = "gs-worker.wasm";
 
 function getBinarySync(file) {
   if (file == wasmBinaryFile && wasmBinary) {
